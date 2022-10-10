b71ebcd2f216dcd5faf3827428855b23872f9b57
Khodor Hassoun <khodorhassoun@outlook.com>
2022-10-11 00:29:39 +0300

    Frontend: succesful connection with backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import Sidebar from './components/Sidebar'
-// import axios from '../node_modules/axios'
-const axios = require('axios');
+import axios from 'axios'
+// const axios = require('axios');
 function App() {
+  axios.get('http://localhost:8000/api/test').then(res=>{console.log(res)})
   const array=[
     {text:'hi', destination:'hello'},
     {text:'hi', destination:'hello'},
