fa81861eba51568b57e5e1c64ed755711064ac3e
Khodor Hassoun <khodorhassoun@outlook.com>
2022-10-11 10:23:54 +0300

    Frontend: use BrowserRouter

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,8 +2,17 @@ import './App.css';
 import Navbar from './components/Navbar';
 import Sidebar from './components/Sidebar'
 import { baseURL } from './helper';
-import axios from 'axios'
+import axios from 'axios';
 import { useEffect } from 'react';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+
+
+
+
+
+
+
+
 function App() {
   useEffect(()=>{
     axios.get(`${baseURL}/test`).then(res => console.log(res))
@@ -11,12 +20,15 @@ function App() {
   
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <Navbar />
-        <Sidebar  />
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <header className="App-header">
+          <Navbar />
+          <Sidebar  />
+        </header>
+      </div>  
+    </BrowserRouter>
+    
   );
 }
 
